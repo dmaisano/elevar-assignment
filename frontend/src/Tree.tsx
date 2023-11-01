c61264be21f94cb49fb86521fe4e8aeca937d68d
Dom Maisano <dmaisanooo@gmail.com>
2023-11-01 19:19:52 -0400

    wip

--- frontend/src/Tree.tsx
+++ frontend/src/Tree.tsx
@@ -10,7 +10,14 @@ interface TreeProps {
 }
 
 const Tree: React.FC<TreeProps> = ({ nodes }) => {
-  return nodes.map(({ text }) => <div>{text}</div>)
+  return nodes.map(({ text, children }) => (
+    <div>
+      <div>{text}</div>
+      {children && children?.length > 0
+        ? children?.map((childNodes) => <Tree nodes={[childNodes]} />)
+        : null}
+    </div>
+  ))
 }
 
 export default Tree
